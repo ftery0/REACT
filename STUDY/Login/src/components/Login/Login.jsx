93fa04dde0e349320036bad44dc2e8180bce9af1
해준 <127281057+ftery0@users.noreply.github.com>
2023-12-05 16:49:47 +0900

    design :: 로그인 화면 변경

--- STUDY/Login/src/components/Login/Login.jsx
+++ STUDY/Login/src/components/Login/Login.jsx
@@ -3,6 +3,7 @@ import "./Login.css";
 import { loginUser } from "../../constants/Login/Login";
 import { showToast } from "../../constants/Swal/Swal_alert";
 import { useNavigate } from "react-router-dom";
+import SOPO from "../../assets/img/SO.png";
 
 export default function Login() {
   const [userid, setUserId] = useState("");
@@ -29,7 +30,7 @@ export default function Login() {
       console.log(password);
       if (success) {
         showToast("success", "로그인 성공");
-        navigate("/Main")
+        navigate("/Main");
       } else {
         showToast("warning", "로그인 실패");
       }
@@ -41,39 +42,50 @@ export default function Login() {
   return (
     <div className="Login">
       <div className="login-group">
-        <h2>로그인</h2>
-        <form action="" className="login_form">
-          <p>
-            <input
-              className="login1"
-              type="text"
-              name="username"
-              placeholder="아이디"
-              onChange={lookingForNum}
-            />
-          </p>
-          <p>
-            <input
-              className="login2"
-              type="password"
-              name="pwd"
-              placeholder="비밀번호"
-              onChange={lookingForNumInPassword}
-            />
-          </p>
+        <div class="logo">
+        <img src={SOPO} />
+        </div>
+        <div className="sc-breuTDiVspsI">
+          <div class="Main_login">
+            
+            <form action="" className="login_form">
+            <h1>로그인</h1>
+            <div className="loginid">
+              <div className="loginidtext">ID</div>
+                <input
+                  className="login1"
+                  type="text"
+                  name="username"
+                  onChange={lookingForNum}
+                />
+              
+              </div>
+              <div className="loginpasword">
+              <div className="loginpaswordtext">비밀번호</div>
+                <input
+                  className="login2"
+                  type="password"
+                  name="pwd"
+                  onChange={lookingForNumInPassword}
+                />
+              
+              </div>
 
-          <input
-            type="button"
-            className="submit"
-            value="로그인"
-            onClick={handleLogin}
-          />
-        </form>
-        <div className="signup-link">
-          <p>
-            계정이 없으신가요?{" "}
-            <button onClick={() => navigate("/signup")}>회원가입</button>
-          </p>
+              <input
+                type="button"
+                className="submit"
+                value="로그인"
+                onClick={handleLogin}
+              />
+              <div className="signup-link">
+              <p>
+                계정이 없으신가요?{" "}
+                <button onClick={() => navigate("/signup")}>회원가입</button>
+              </p>
+            </div>
+            </form>
+            
+          </div>
         </div>
       </div>
     </div>
