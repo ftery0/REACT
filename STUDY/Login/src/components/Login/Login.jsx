af6e5404bba90034f2baeb2ddefcdab50aa84b28
해준 <127281057+ftery0@users.noreply.github.com>
2023-12-07 10:59:31 +0900

    design :: 로그인

--- STUDY/Login/src/components/Login/Login.jsx
+++ STUDY/Login/src/components/Login/Login.jsx
@@ -3,7 +3,7 @@ import "./Login.css";
 import { loginUser } from "../../constants/Login/Login";
 import { showToast } from "../../constants/Swal/Swal_alert";
 import { useNavigate } from "react-router-dom";
-import SOPO from "../../assets/img/SO.png";
+import LOGO from "../../assets/img/Lofo.png";
 
 export default function Login() {
   const [userid, setUserId] = useState("");
@@ -43,32 +43,29 @@ export default function Login() {
     <div className="Login">
       <div className="login-group">
         <div class="logo">
-        <img src={SOPO} />
+          <img src={LOGO} />
         </div>
         <div className="sc-breuTDiVspsI">
           <div class="Main_login">
-            
             <form action="" className="login_form">
-            <h1>로그인</h1>
-            <div className="loginid">
-              <div className="loginidtext">ID</div>
+              <h1>로그인</h1>
+              <div className="loginid">
+                <div className="idtext">ID</div>
                 <input
                   className="login1"
                   type="text"
                   name="username"
                   onChange={lookingForNum}
                 />
-              
               </div>
               <div className="loginpasword">
-              <div className="loginpaswordtext">비밀번호</div>
+                <div className="paswordtext">비밀번호</div>
                 <input
                   className="login2"
                   type="password"
                   name="pwd"
                   onChange={lookingForNumInPassword}
                 />
-              
               </div>
 
               <input
@@ -78,13 +75,12 @@ export default function Login() {
                 onClick={handleLogin}
               />
               <div className="signup-link">
-              <p>
-                계정이 없으신가요?{" "}
-                <button onClick={() => navigate("/signup")}>회원가입</button>
-              </p>
-            </div>
+                <p>
+                  계정이 없으신가요?{" "}
+                  <button onClick={() => navigate("/signup")}>회원가입</button>
+                </p>
+              </div>
             </form>
-            
           </div>
         </div>
       </div>
