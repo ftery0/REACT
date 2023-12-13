b9f1c56f4b0030b99a0bcc21e102e872192eb632
해준 <127281057+ftery0@users.noreply.github.com>
2023-12-13 17:04:34 +0900

    feat :: Main navigation bar

--- STUDY/Login/src/components/Main/main.jsx
+++ STUDY/Login/src/components/Main/main.jsx
@@ -1,10 +1,48 @@
-import React from "react"
-import "./main.css"
+import React, { Component } from "react";
+import "./main.css";
+import Menu from "../../assets/img/dehaze.png";
 
-export default function(){
-    return(
-        <div className="Main">
-            <h1>main</h1>
-        </div>
-    )
+class Main extends Component {
+    state = {
+        isMenuVisible: true
+    };
+
+    menuClickTR = () => {
+        this.setState({ isMenuVisible: false }); 
+    };
+    menuClickFA = () => {
+
+        this.setState({ isMenuVisible: true})
+    }
+
+    render() {
+        return (
+            <div className="Main">
+                <header className="header">
+                    <h1 className="LOGO">TEST</h1>
+                    {this.state.isMenuVisible && (
+                        <div className="menuHeader" onClick={this.menuClickTR}>
+                            <img src={Menu} alt="Menu"/>
+                        </div>
+                    )}
+                     {!this.state.isMenuVisible && (
+                    <div className="MenuotherDiv">
+                        <div className="menuHeader" onClick={this.menuClickFA}>
+                            <img src={Menu} alt="Menu"/>
+                        </div>
+                        <div className="menuBar">
+                            <div className="MenuBarCT" id="MENUNUMBER1">내정보</div>
+                            <div className="MenuBarCT" id="MENUNUMBER2">작품</div>
+                            <div className="MenuBarCT" id="MENUNUMBER2">추억</div>
+                        </div>
+                        
+                    </div>
+                )}
+                </header>
+               
+            </div>
+        );
+    }
 }
+
+export default Main;
