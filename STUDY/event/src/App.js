38af4fe7e4b94a9173b8aa5e785a9db3bfb287a3
해준 <127281057+ftery0@users.noreply.github.com>
2024-01-24 22:08:33 +0900

    set up

--- STUDY/event/src/App.js
+++ STUDY/event/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Main from './components/Home/Main/Main';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-  
+    <Routes>
+      <Route path='/' element={<Main/>}></Route>
+    </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
