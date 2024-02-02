0694710f9a84d00ee6126af55b9cc4d2bd77e1cc
해준 <127281057+ftery0@users.noreply.github.com>
2024-02-02 23:01:43 +0900

    test :: 테스트이미지

--- STUDY/event/src/components/Home/Main/Main.jsx
+++ STUDY/event/src/components/Home/Main/Main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import"./main.css";
 import Head from "../../Head/header";
+import BackGround from "../../../assets/000058270012.jpg"
 
 export default function Main() {
   return (
     <div className="Main" src={"sd"}>
       <Head />
+      <img src={BackGround} alt="" />
     </div>
   )    
 }
